692b182357a9e973e520cd01bb54f3213b25009b
Dave <obeed06@gmail.com>
2025-09-19 15:57:56 +0100

    update to sanity studio 4.9

---
+++ studio/sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure' 
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas/schema'
+import {colorInput} from '@sanity/color-input'
+
+export default defineConfig({
+  name: 'default',
+  title: 'react-travel-blog',
+
+  projectId: 'ho3u0oh3',
+  dataset: 'production',
+
+  plugins: [colorInput(),structureTool(), visionTool()], 
+
+  schema: {
+    types: schemaTypes, 
+  },
+})
