451744ae99da909f6e605fe43a6426b79f0511f5
Dave <obeed06@gmail.com>
2025-09-19 19:17:39 +0100

    load world-countries.json from public assets

--- components/mapChart/MapChart.js
+++ components/mapChart/MapChart.js
@@ -1,5 +1,5 @@
 import styles from './MapChart.module.css';
-import React, {memo, useContext} from "react";
+import { memo, useContext, useState, useEffect } from "react";
 import {
     ComposableMap,
     Geographies,
@@ -9,44 +9,47 @@ import ColorModeContext from "../../src/ColorModeContext";
 
 const excluded = ["Antarctica"]
 
-const geoUrl =
-    "../world-countries.json";
-
-const MapChart = ({visitedGeos}) => {
+const MapChart = ({ visitedGeos }) => {
     const { darkMode } = useContext(ColorModeContext);
+    const [geoData, setGeoData] = useState(null);
+    useEffect(() => {
+        fetch('/assets/world-countries.json')
+            .then((res) => res.json())
+            .then((data) => setGeoData(data));
+    }, []);
 
-    let defaultSVGColorFill = darkMode ? "#23252b" : "#efefef" ;
+    let defaultSVGColorFill = darkMode ? "#23252b" : "#efefef";
 
     return (
         visitedGeos ?
             (
-                    <ComposableMap className={styles.mapChart} data-tip="" projection="geoEqualEarth"
-                                   projectionConfig={{scale: 180}}>
-                        <Geographies geography={geoUrl}>
-                            {({geographies}) =>
-                                geographies.filter(geo => !excluded.includes(geo.properties['name'])).map(geo => (
-                                    <Geography
-                                        key={geo.rsmKey}
-                                        geography={geo}
-                                        style={{
-                                            default: {
-                                                fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
-                                                outline: "none"
-                                            },
-                                            hover: {
-                                                fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
-                                                outline: "none"
-                                            },
-                                            pressed: {
-                                                fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
-                                                outline: "none"
-                                            }
-                                        }}
-                                    />
-                                ))
-                            }
-                        </Geographies>
-                    </ComposableMap>
+                <ComposableMap className={styles.mapChart} data-tip="" projection="geoEqualEarth"
+                    projectionConfig={{ scale: 180 }}>
+                    <Geographies geography={geoData}>
+                        {({ geographies }) =>
+                            geographies.filter(geo => !excluded.includes(geo.properties['name'])).map(geo => (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    style={{
+                                        default: {
+                                            fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
+                                            outline: "none"
+                                        },
+                                        hover: {
+                                            fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
+                                            outline: "none"
+                                        },
+                                        pressed: {
+                                            fill: (visitedGeos.includes(geo.properties['name']) ? "var(--brand-color)" : defaultSVGColorFill),
+                                            outline: "none"
+                                        }
+                                    }}
+                                />
+                            ))
+                        }
+                    </Geographies>
+                </ComposableMap>
             ) : (
                 <></>
             )
